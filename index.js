d2f6482f097ce63681c749ea3cf5bbcecaf9bc8c
Dmitry <dzmitry_mamyrau@epam.com>
2017-01-09 23:07:17 +0300

    commander added

---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const git = require('simple-git')();
+const program = require('commander');
+
+program
+  .option('-b, --base_branch [branch]', 'base branch')
+  .option('-n, --pr_branch [branch]', 'pr branch')
+  .action((options) => {
+    
+  })
+  .parse(process.argv);
+
+console.log(program);
+
+// git checkout release/4.3.3 && 
+// git pull &&
+// git checkout feature/4.3.3/epg-performance-improvements-HZN4UI-12253 &&
+// git pull origin feature/4.3.3/epg-performance-improvements-HZN4UI-12253 && 
+// npm run build-prod && 
+// npm run restart
